564f352edf34136d0cf2e156337e771180621b10
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-08 19:48:43 +0100

    Create LIBRARY.py

---
+++ LIBRARY.py
@@ -0,0 +1,6 @@
+import nltk                                # Python library for NLP
+from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt            # library for visualization
+import random                              # pseudo-random number generator
+
+nltk.download('twitter_samples')           # downloads sample twitter dataset.
