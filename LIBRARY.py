ce4ad1f478004046e17214c01a54d4d4ae899b8a
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-08 20:30:47 +0100

    Update LIBRARY.py

--- LIBRARY.py
+++ LIBRARY.py
@@ -11,3 +11,19 @@ print('Number of negative tweets: ', len(all_negative_tweets))
 
 print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
 print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+
+
+fig = plt.figure(figsize=(5, 5))     # Declare a figure with a custom size
+
+labels = 'Positives', 'Negative'     # labels for the two classes
+
+sizes = [len(all_positive_tweets), len(all_negative_tweets)] # Sizes for each slide
+
+
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90) # Declare pie chart, where the slices will be ordered and plotted counter-clockwise
+
+
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+
+plt.show() # Display the chart
