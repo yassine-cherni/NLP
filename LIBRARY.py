97bf409ff4030dca2919558cfe2cc2ba4a4f355e
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-08 20:09:29 +0100

    Update LIBRARY.py

--- LIBRARY.py
+++ LIBRARY.py
@@ -4,3 +4,5 @@ import matplotlib.pyplot as plt            # library for visualization
 import random                              # pseudo-random number generator
 
 nltk.download('twitter_samples')           # downloads sample twitter dataset.
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')                          # select the set of positive and negative tweets
