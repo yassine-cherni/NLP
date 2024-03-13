51f1e8a67507e2e78b680a60faf5b5ee0adc51fd
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-13 01:56:17 +0100

    Update LIBRARY.py

--- LIBRARY.py
+++ LIBRARY.py
@@ -1,7 +1,9 @@
 import nltk                                # Python library for NLP
 from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
 import matplotlib.pyplot as plt            # library for visualization
-import random                              # pseudo-random number generator
+import random    # pseudo-random number generator
+import time
+
 
 nltk.download('twitter_samples')           # downloads sample twitter dataset.
 all_positive_tweets = twitter_samples.strings('positive_tweets.json')
