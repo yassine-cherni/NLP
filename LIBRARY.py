8d9de6c9c47144c821711d7b42a99ffd92811cdd
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-15 19:40:22 +0100

    Update LIBRARY.py

--- LIBRARY.py
+++ LIBRARY.py
@@ -1,7 +1,7 @@
 import nltk                                # Python library for NLP
 from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
 import matplotlib.pyplot as plt            # library for visualization
-import random    # pseudo-random number generator
+import rand    # pseudo-random number generator
 import time
 import os
 import tensorflow 
